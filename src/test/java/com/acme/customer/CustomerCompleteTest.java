7569bda2464b4132e07af11e60a825a42fb394fe
Isaac Goldstein <isaac.goldstein@ny.frb.org>
2025-08-07 11:43:50 -0400

    reorg imports to use junit, not jupiter

--- src/test/java/com/acme/customer/CustomerCompleteTest.java
+++ src/test/java/com/acme/customer/CustomerCompleteTest.java
@@ -1,8 +1,8 @@
 package com.acme.customer;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
